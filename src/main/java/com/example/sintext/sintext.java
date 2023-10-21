ebd89aaaf612f727b250ef3bf0af28ce2c97129c
Tanishq Dhote <tanishqbakka1@gmail.com>
2023-10-21 09:43:04 +0530

    Update sintext.java

--- src/main/java/com/example/sintext/sintext.java
+++ src/main/java/com/example/sintext/sintext.java
@@ -6,19 +6,30 @@ import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
+import javafx.scene.control.Button;
 public class sintext extends Application {
     @Override
     public void start(Stage stage){
+        Group root = new Group();
+        Scene scene = new Scene(root, 960, 540);
+        scene.getStylesheets().add("./app.css");
+
 
         VBox vb1 = new VBox();
+        vb1.setId("vb1");
+        Button b1= new Button("Click Me!");
+        b1.setStyle("-fx-background-color:blue");
+        vb1.getChildren().addAll(b1);
+        b1.setId("b1");
         VBox vb2 = new VBox();
+        vb2.setId("vb2");
         HBox master = new HBox();
+        master.setId("master");
         master.getChildren().addAll(vb1,vb2);
 
-        Group root = new Group();
+
         root.getChildren().addAll(master);
-        Scene scene = new Scene(root, 960, 540);
+
         stage.setTitle("SINTEXT 2.0");
         stage.setScene(scene);
         stage.show();
